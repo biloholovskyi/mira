604c9747dee4c8bde3c8457bcfea7c960d296767
Biloholovskyi <biloholovskyi>
2021-06-11 14:32:42 +0300

    some fix

--- src/components/pages/authorizationCode/singleCodeItem/singleCodeItem.js
+++ src/components/pages/authorizationCode/singleCodeItem/singleCodeItem.js
@@ -2,22 +2,28 @@ import React, {Component} from "react";
 
 import {CodeItem, Border} from '../style';
 
-class SingleCodeItem extends Component{
+class SingleCodeItem extends Component {
 
- focusNextField(nextField) {
-    this.refs[nextField].focus();
+
+  focusNextField(nextField) {
+    this.refs[nextField - 1].value && this.refs[nextField].focus();
   }
 
   render() {
     return (
       <>
-        <CodeItem  ref="1"  onChange={() => this.focusNextField('2')} type={'text'} name={'code'} maxLength={'1'} required autoComplete={'off'}/>
-        <CodeItem  ref="2"  onChange={() => this.focusNextField('3')} type={'text'} name={'code'} maxLength={'1'} required autoComplete={'off'}/>
-        <CodeItem  ref="3"  onChange={() => this.focusNextField('4')} type={'text'} name={'code'} maxLength={'1'} required autoComplete={'off'}/>
-        <Border />
-        <CodeItem  ref="4"  onChange={() => this.focusNextField('5')} type={'text'} name={'code'} maxLength={'1'} required autoComplete={'off'}/>
-        <CodeItem  ref="5"  onChange={() => this.focusNextField('6')} type={'text'} name={'code'} maxLength={'1'} required autoComplete={'off'}/>
-        <CodeItem  ref="6"   type={'text'} name={'code'} maxLength={'1'} required autoComplete={'off'}/>
+        <CodeItem ref="1" onChange={() => this.focusNextField('2')} type={'text'} name={'code'} maxLength={'1'} required
+                  autoComplete={'off'}/>
+        <CodeItem ref="2" onChange={() => this.focusNextField('3')} type={'text'} name={'code'} maxLength={'1'} required
+                  autoComplete={'off'}/>
+        <CodeItem ref="3" onChange={() => this.focusNextField('4')} type={'text'} name={'code'} maxLength={'1'} required
+                  autoComplete={'off'}/>
+        <Border/>
+        <CodeItem ref="4" onChange={() => this.focusNextField('5')} type={'text'} name={'code'} maxLength={'1'} required
+                  autoComplete={'off'}/>
+        <CodeItem ref="5" onChange={() => this.focusNextField('6')} type={'text'} name={'code'} maxLength={'1'} required
+                  autoComplete={'off'}/>
+        <CodeItem ref="6" type={'text'} name={'code'} maxLength={'1'} required autoComplete={'off'}/>
       </>
     )
   }
